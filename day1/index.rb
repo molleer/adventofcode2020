71a97084036479c9214d2e82a0204a2cdfb6f448
David Möller <damoller99@hotmail.se>
2020-12-01 07:34:00 +0100

    Solved day1

--- day1/index.rb
+++ day1/index.rb
@@ -1,6 +1,13 @@
-sum = 0
-for value in File.read("input.txt").split do
-  sum += value.to_i
-end
+values = File.read("input.txt").split
+found = false
 
-print sum.to_s + "\n"
+for i in  0..(values.length()-1)
+  for j in (i+1)..(values.length()-1)
+    for k in (j+1)..(values.length()-1)
+      if values[i].to_i + values[j].to_i + values[k].to_i == 2020
+        puts (values[i].to_i * values[j].to_i * values[k].to_i).to_s
+        return
+      end
+    end
+  end
+end
