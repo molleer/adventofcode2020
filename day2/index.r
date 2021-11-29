6bb2dd61b5c17d157864856bf62369d25576f6a3
David Möller <davidm@student.chalmers.se>
2021-11-29 20:48:56 +0100

    Pushing changes

--- day2/index.r
+++ day2/index.r
@@ -5,7 +5,7 @@ passes <- 0
 
 for(line in input) {
     parts <- str_split(line, " ")[[1]]
-    bounds <- strtoi(str_split(line, "-")[[1]])
+    bounds <- strtoi(str_split(parts[1], "-")[[1]])
     c <- substr(parts[2], 1, 1)
     count <- 0
     for(i in str_split(parts[3], "")[[1]]) {
@@ -13,8 +13,23 @@ for(line in input) {
         count <- count + 1
       }
     }
-    if((count >= bounds[1]) && (count <= bounds[2])) {
+    if(count >= bounds[1] && count <= bounds[2]) {
       passes <- passes + 1
     }
+}
+print(passes)
+
+passes <- 0
+
+for(line in input) {
+  parts <- str_split(line, " ")[[1]]
+  bounds <- strtoi(str_split(parts[1], "-")[[1]])
+  c <- substr(parts[2], 1, 1)
+  count <- 0
+  pass <- str_split(parts[3], "")[[1]]
+  
+  if(xor(pass[bounds[1]] == c, pass[bounds[2]] == c)) {
+    passes <- passes + 1
+  }
 }
 print(passes)
